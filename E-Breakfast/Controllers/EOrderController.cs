adc8ec003225f5a5a09965d57b9b9f6e94a391b8
Admin <hanh1997haui@gmail.com>
2025-02-22 23:18:11 +0700

    hanhtd cm

---
+++ E-Breakfast/Controllers/EOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Breakfast.Controllers
+{
+    public class EOrderController : Controller
+    {
+        public IActionResult AddNewOrder()
+        {
+            return View();
+        }
+    }
+}
